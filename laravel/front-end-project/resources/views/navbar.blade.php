9662927ea62986ab4626388b73bbee9d355a6f9c
aammmmm <putralukasa@gmail.com>
2024-03-18 15:22:47 +0700

    navbar login

--- laravel/front-end-project/resources/views/navbar.blade.php
+++ laravel/front-end-project/resources/views/navbar.blade.php
@@ -14,36 +14,70 @@
     }
 @endphp
 
+@if(session()->has('user'))
 <header>
-  <nav class="navbar navbar-expand-lg navbar-{{ $navbarColor }} fixed-top p-4">
-      <div class="container-fluid">
-          <a class="navbar-brand pe-4" href="/">Hayden</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link px-4 {{ Request::is('/') ? 'active' : '' }}" href="/">Portfolio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link px-4 {{ Request::is('packagechoose') ? 'active' : '' }}" href="/packagechoose">Package</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link px-4 {{ Request::is('gownchoose') ? 'active' : '' }}" href="/gownchoose">Busana</a>
-                </li>
-            </ul>
-        </div>
-          <div class="ml-auto d-flex align-items-center">
-              <a class="nav-link me-2 pe-4 text-{{ $navbarColor2 }}" href="/signup">Daftar</a>
-              {{-- <button class="btn btn-custom" style="width: 130px; height: 62px; color: white">Masuk</button> --}}
-              <a href="/signin" class="btn btn-custom d-flex justify-content-center align-items-center" style="width: 130px; height: 62px; color: white; box-shadow: 0 4px 10px rgba(255,105,105, 0.4)">
-                <span style="margin: auto; font-size: 18px; font-weight: 500">Masuk</span>
-            </a>            
+    <nav class="navbar navbar-expand-lg navbar-{{ $navbarColor }} fixed-top p-4">
+        <div class="container-fluid">
+            <a class="navbar-brand pe-4" href="/">Hayden</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                  <li class="nav-item">
+                      <a class="nav-link px-4 {{ Request::is('/') ? 'active' : '' }}" href="/">Portfolio</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link px-4 {{ Request::is('packagechoose') ? 'active' : '' }}" href="/packagechoose">Package</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link px-4 {{ Request::is('gownchoose') ? 'active' : '' }}" href="/gownchoose">Busana</a>
+                  </li>
+              </ul>
           </div>
-      </div>
-  </nav>
-</header>
+            <div class="ml-auto d-flex align-items-center">
+                {{-- <a class="nav-link me-2 pe-4 text-{{ $navbarColor2 }}" href="/signup">Daftar</a> --}}
+                {{-- <button class="btn btn-custom" style="width: 130px; height: 62px; color: white">Masuk</button> --}}
+                <a href="/signin" class="btn btn-custom d-flex justify-content-center align-items-center" style="width: 130px; height: 62px; color: white; box-shadow: 0 4px 10px rgba(255,105,105, 0.4)">
+                  <span style="margin: auto; font-size: 18px; font-weight: 500">Pesen</span>
+              </a>            
+            </div>
+        </div>
+    </nav>
+  </header>
+@else 
+<header>
+    <nav class="navbar navbar-expand-lg navbar-{{ $navbarColor }} fixed-top p-4">
+        <div class="container-fluid">
+            <a class="navbar-brand pe-4" href="/">Hayden</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                  <li class="nav-item">
+                      <a class="nav-link px-4 {{ Request::is('/') ? 'active' : '' }}" href="/">Portfolio</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link px-4 {{ Request::is('packagechoose') ? 'active' : '' }}" href="/packagechoose">Package</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link px-4 {{ Request::is('gownchoose') ? 'active' : '' }}" href="/gownchoose">Busana</a>
+                  </li>
+              </ul>
+          </div>
+            <div class="ml-auto d-flex align-items-center">
+                <a class="nav-link me-2 pe-4 text-{{ $navbarColor2 }}" href="/signup">Daftar</a>
+                {{-- <button class="btn btn-custom" style="width: 130px; height: 62px; color: white">Masuk</button> --}}
+                <a href="/signin" class="btn btn-custom d-flex justify-content-center align-items-center" style="width: 130px; height: 62px; color: white; box-shadow: 0 4px 10px rgba(255,105,105, 0.4)">
+                  <span style="margin: auto; font-size: 18px; font-weight: 500">Masuk</span>
+              </a>            
+            </div>
+        </div>
+    </nav>
+  </header>
+@endif
+
 
 
 
--- laravel/front-end-project/storage/framework/views/e5893a84f375a1cf3f123a0f0218eb9e.php
+++ laravel/front-end-project/storage/framework/views/e5893a84f375a1cf3f123a0f0218eb9e.php
@@ -18,7 +18,6 @@
 
     <?php echo $__env->make('footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
-    
 </body>
 </html>
 <?php /**PATH /Users/aam/Desktop/Project Skripsi/FE/projectskripsife/laravel/front-end-project/resources/views/template.blade.php ENDPATH**/ ?>
