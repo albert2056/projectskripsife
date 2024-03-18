fbd5765a0e684a63135d343db0b1037b6bbbf99f
albert.akira <albert.akira@gdn-commerce.com>
2024-03-18 21:37:01 +0700

    create home page

--- laravel/front-end-project/resources/views/Home.blade.php
+++ laravel/front-end-project/resources/views/Home.blade.php
@@ -10,7 +10,106 @@
     </ul>
 </div> --}}
 
-<img class= "bg-image" src="https://images.unsplash.com/photo-1505935428862-770b6f24f629?ixlib=rb-4.0.3&ixid=" alt="">
+<link rel="stylesheet" href="{{asset('css/guesthome.css')}}"/>
+<div class="container1">
+    <div class="section1">
+        <span class="home-main-page-text">
+            <span class="home-main-page-text1">
+                <span>Wedding Usher</span>
+                <br />
+                <span></span>
+            </span>
+        <span class="home-main-page-text2">Dream Wedding come true</span>
+      </span>
+      <img
+        src="external/vector6337-geyo.svg"
+        alt="Vector6337"
+        class="home-main-page-love"
+      />
+      <span class="home-main-page-text3">
+        <span>
+          <span>Membantu memberikan kesan elegan dan professional</span>
+          <br />
+          <span>pada pernikahan</span>
+        </span>
+      </span>
+      <img
+        src="external/rectangle116342-pn5m-200w.png"
+        alt="Rectangle116342"
+        class="home-main-page-rectangle"
+      />
+    </div>
+    <div class="section2">
+        <span class="home-main-page-text4">
+          <span>Pesan Sekarang</span>
+        </span>
+    </div>
+    <div class="section3">
+        <div class="home-main-page-frame14287">
+          <div class="home-main-page-frame14284">
+            <div class="home-main-page-group12">
+              <img
+                src="external/ellipse88637-5xc-200h.png"
+                alt="Ellipse88637"
+                class="home-main-page-ellipse8"
+              />
+              <img
+                src="external/bridesmaid11101-o186-200h.png"
+                alt="bridesmaid11101"
+                class="home-main-page-bridesmaid1"
+              />
+            </div>
+            <span class="home-main-page-text33"><span>40+</span></span>
+            <span class="home-main-page-text35">
+              <span>Jumlah Usher Kami</span>
+            </span>
+          </div>
+          <div class="home-main-page-frame14285">
+            <div class="home-main-page-group121">
+              <img
+                src="external/ellipse88638-9s7-200h.png"
+                alt="Ellipse88638"
+                class="home-main-page-ellipse81"
+              />
+              <img
+                src="external/weddingcouple11101-r8q8-200h.png"
+                alt="weddingcouple11101"
+                class="home-main-page-weddingcouple1"
+              />
+            </div>
+            <span class="home-main-page-text37"><span>200+</span></span>
+            <span class="home-main-page-text39">
+              <span>Jumlah Klien</span>
+            </span>
+          </div>
+          <div class="home-main-page-frame14286">
+            <div class="home-main-page-group122">
+              <img
+                src="external/ellipse88638-gplg-200h.png"
+                alt="Ellipse88638"
+                class="home-main-page-ellipse82"
+              />
+              <img
+                src="external/wedding12541-7mym-200h.png"
+                alt="wedding12541"
+                class="home-main-page-wedding1"
+              />
+            </div>
+            <span class="home-main-page-text41"><span>100+</span></span>
+            <span class="home-main-page-text43">
+              <span>Jumlah Venue</span>
+            </span>
+          </div>
+        </div>
+      </div>
+      <button class="home-main-page-divczmbutton">
+        <div class="home-main-page-ifawhatsapp">
+          <img src="external/wawhite.png" class="home-main-page-text45"></img>
+        </div>
+        <span class="home-main-page-text46"><span>&nbsp;Chat</span></span>
+      </button>
+</div>
+
 
 
 {{-- <img class= "bg-image" src="{{ asset('bgHome.png') }}" alt=""> --}}
