11aa60338eb6703a86494c22c53d77e3d318db22
albert.akira <albert.akira@gdn-commerce.com>
2024-03-18 16:24:59 +0700

    remove link from login and register implement template and add href

---
+++ laravel/front-end-project/storage/framework/views/5ffe7ef79eacf40a029caa375b81df76.php
@@ -0,0 +1,95 @@
+<?php
+     $navbarColor = ''; 
+     $navbarColor2 = '';
+    if (Route::currentRouteName() == 'home') {
+        $navbarColor = 'dark';
+    } else {
+        $navbarColor = 'light';
+    }
+
+    if (Route::currentRouteName() == 'home') {
+        $navbarColor2 = 'light';
+    } else {
+        $navbarColor2 = 'dark';
+    }
+?>
+
+<?php if(session()->has('user')): ?>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-<?php echo e($navbarColor); ?> fixed-top p-4">
+        <div class="container-fluid">
+            <a class="navbar-brand pe-4" href="/">Hayden</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                  <li class="nav-item">
+                      <a class="nav-link px-4 <?php echo e(Request::is('/') ? 'active' : ''); ?>" href="/">Portfolio</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link px-4 <?php echo e(Request::is('packagechoose') ? 'active' : ''); ?>" href="/packagechoose">Package</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link px-4 <?php echo e(Request::is('gownchoose') ? 'active' : ''); ?>" href="/gownchoose">Busana</a>
+                  </li>
+              </ul>
+          </div>
+            <div class="ml-auto d-flex align-items-center">
+                
+                
+                <a href="/signin" class="btn btn-custom d-flex justify-content-center align-items-center" style="width: 130px; height: 62px; color: white; box-shadow: 0 4px 10px rgba(255,105,105, 0.4)">
+                  <span style="margin: auto; font-size: 18px; font-weight: 500">Pesen</span>
+              </a>            
+            </div>
+        </div>
+    </nav>
+  </header>
+<?php else: ?> 
+<header>
+    <nav class="navbar navbar-expand-lg navbar-<?php echo e($navbarColor); ?> fixed-top p-4">
+        <div class="container-fluid">
+            <a class="navbar-brand pe-4" href="/">Hayden</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                  <li class="nav-item">
+                      <a class="nav-link px-4 <?php echo e(Request::is('/') ? 'active' : ''); ?>" href="/">Portfolio</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link px-4 <?php echo e(Request::is('packagechoose') ? 'active' : ''); ?>" href="/packagechoose">Package</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link px-4 <?php echo e(Request::is('gownchoose') ? 'active' : ''); ?>" href="/gownchoose">Busana</a>
+                  </li>
+              </ul>
+          </div>
+            <div class="ml-auto d-flex align-items-center">
+                <a class="nav-link me-2 pe-4 text-<?php echo e($navbarColor2); ?>" href="/signup">Daftar</a>
+                
+                <a href="/signin" class="btn btn-custom d-flex justify-content-center align-items-center" style="width: 130px; height: 62px; color: white; box-shadow: 0 4px 10px rgba(255,105,105, 0.4)">
+                  <span style="margin: auto; font-size: 18px; font-weight: 500">Masuk</span>
+              </a>            
+            </div>
+        </div>
+    </nav>
+  </header>
+<?php endif; ?>
+
+
+
+
+<script>
+    const navEL = document.querySelector('.navbar'); //nampung kelas navbar ke variabel navEL
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY >= 56) {
+            navEL.classList.add('navbar-scrolled')
+        }else if(window.scrollY < 56) {
+            navEL.classList.remove('navbar-scrolled')
+        }
+    })
+  </script>
+<?php /**PATH D:\Documents\project skripsi\with git\front end\project skripsi\laravel\front-end-project\resources\views/navbar.blade.php ENDPATH**/ ?>
--- laravel/front-end-project/storage/framework/views/98c0fab42ad92cb616d3d97decd85e5d.php
+++ laravel/front-end-project/storage/framework/views/98c0fab42ad92cb616d3d97decd85e5d.php
@@ -1,235 +1,139 @@
-<!DOCTYPE html>
-<html lang="english">
-  <head>
-    <title>Register</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta charset="utf-8" />
-    <meta property="twitter:card" content="summary_large_image" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <style data-tag="reset-style-sheet">
-      html {  line-height: 1.15;}body {  margin: 0;}* {  box-sizing: border-box;  border-width: 0;  border-style: solid;}p,li,ul,pre,div,h1,h2,h3,h4,h5,h6,figure,blockquote,figcaption {  margin: 0;  padding: 0;}button {  background-color: transparent;}button,input,optgroup,select,textarea {  font-family: inherit;  font-size: 100%;  line-height: 1.15;  margin: 0;}button,select {  text-transform: none;}button,[type="button"],[type="reset"],[type="submit"] {  -webkit-appearance: button;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner {  border-style: none;  padding: 0;}button:-moz-focus,[type="button"]:-moz-focus,[type="reset"]:-moz-focus,[type="submit"]:-moz-focus {  outline: 1px dotted ButtonText;}a {  color: inherit;  text-decoration: inherit;}input {  padding: 2px 4px;}img {  display: block;}html { scroll-behavior: smooth  }
-    </style>
-    <style data-tag="default-style-sheet">
-      html {
-        font-family: Inter;
-        font-size: 16px;
-      }
 
-      body {
-        font-weight: 400;
-        font-style:normal;
-        text-decoration: none;
-        text-transform: none;
-        letter-spacing: normal;
-        line-height: 1.15;
-        color: var(--dl-color-gray-black);
-        background-image: url('external/loginbg.jpg');
-        background-size: cover;
-        background-position: center;
-        height: 100vh; 
-      }
-    </style>
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/animate.css@4.1.1/animate.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-      data-tag="font"
-    />
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/@teleporthq/teleport-custom-scripts/dist/style.css"
-    />
-  </head>
-  <body>
-    <link rel="stylesheet" href=<?php echo e(asset('css/registerstyle.css')); ?>/>
-    <div>
-      <link href=<?php echo e(asset('css/registerindex.css')); ?> rel="stylesheet" />
-      <form action="<?php echo e(route('register')); ?>" method="post">
-        <?php echo csrf_field(); ?>
-      <div class="createanaccount-container">
-        <div class="createanaccount-createanaccount">
-          <div class="createanaccount-frame352">
-            <div class="createanaccount-createanaccount1">
-              <div class="createanaccount-frame353">
-                <span class="createanaccount-text">
-                  <span>Create an account</span>
+
+<?php $__env->startSection('content'); ?>
+  <style data-tag="reset-style-sheet">
+    html {  line-height: 1.15;}body {  margin: 0;}* {  box-sizing: border-box;  border-width: 0;  border-style: solid;}p,li,ul,pre,div,h1,h2,h3,h4,h5,h6,figure,blockquote,figcaption {  margin: 0;  padding: 0;}button {  background-color: transparent;}button,input,optgroup,select,textarea {  font-family: inherit;  font-size: 100%;  line-height: 1.15;  margin: 0;}button,select {  text-transform: none;}button,[type="button"],[type="reset"],[type="submit"] {  -webkit-appearance: button;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,[type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner {  border-style: none;  padding: 0;}button:-moz-focus,[type="button"]:-moz-focus,[type="reset"]:-moz-focus,[type="submit"]:-moz-focus {  outline: 1px dotted ButtonText;}a {  color: inherit;  text-decoration: inherit;}input {  padding: 2px 4px;}img {  display: block;}html { scroll-behavior: smooth  }
+  </style>
+  <style data-tag="default-style-sheet">
+    html {
+      font-family: Inter;
+      font-size: 16px;
+    }
+
+    body {
+      font-weight: 400;
+      font-style:normal;
+      text-decoration: none;
+      text-transform: none;
+      letter-spacing: normal;
+      line-height: 1.15;
+      color: var(--dl-color-gray-black);
+      background-image: url('external/loginbg.jpg');
+      background-size: cover;
+      background-position: center;
+      height: 100vh; 
+      margin-bottom: 10vh;
+      margin-top: 10vh; 
+    }
+  </style>
+  <link rel="stylesheet" href=<?php echo e(asset('css/registerstyle.css')); ?>/>
+  <div>
+    <link href=<?php echo e(asset('css/registerindex.css')); ?> rel="stylesheet" />
+    <form action="<?php echo e(route('register')); ?>" method="post">
+      <?php echo csrf_field(); ?>
+    <div class="createanaccount-container">
+      <div class="createanaccount-createanaccount">
+        <div class="createanaccount-frame352">
+          <div class="createanaccount-createanaccount1">
+            <div class="createanaccount-frame353">
+              <span class="createanaccount-text">
+                <span>Registrasi</span>
+              </span>
+            </div>
+            <div class="createanaccount-haveanaccountlogin">
+              <span class="createanaccount-text02">
+                <span class="createanaccount-text03">
+                  Sudah punya akun?
                 </span>
+                <a href="/signin">Masuk</a>
+              </span>
+            </div>
+            <div class="createanaccount-textfield">
+              <div class="createanaccount-frame243">
+                <span class="createanaccount-text05"><span>Name</span></span>
               </div>
-              <div class="createanaccount-haveanaccountlogin">
-                <span class="createanaccount-text02">
-                  <span class="createanaccount-text03">
-                    Already have an account?
+              <input type="text" name="name" class="createanaccount-textfield1" id="name" value="">
+            </div>
+            <div class="createanaccount-textfield">
+              <div class="createanaccount-frame243">
+                <span class="createanaccount-text05"><span>Email</span></span>
+              </div>
+              <input type="text" name="email" class="createanaccount-textfield1" id="email" value="">
+            </div>
+            <div class="createanaccount-textfield">
+              <div class="createanaccount-frame243">
+                <span class="createanaccount-text05"><span>Phone</span></span>
+              </div>
+              <input type="text" name="phone" class="createanaccount-textfield1" id="phone" value="">
+            </div>
+            <div class="createanaccount-frame358">
+              <div class="createanaccount-textfield">
+                <div class="createanaccount-frame243">
+                  <span class="createanaccount-text05">
+                    <span>Password</span>
                   </span>
-                  <span>Sign in</span>
-                </span>
-              </div>
-              <div class="createanaccount-textfield">
-                <div class="createanaccount-frame243">
-                  <span class="createanaccount-text05"><span>Name</span></span>
+                  
                 </div>
-                <input type="text" name="name" class="createanaccount-textfield1" id="name" value="">
+                <input type="password" name="password" class="createanaccount-textfield1" id="password" value="">
               </div>
-              <div class="createanaccount-textfield">
-                <div class="createanaccount-frame243">
-                  <span class="createanaccount-text05"><span>Email</span></span>
-                </div>
-                <input type="text" name="email" class="createanaccount-textfield1" id="email" value="">
-              </div>
-              <div class="createanaccount-textfield">
-                <div class="createanaccount-frame243">
-                  <span class="createanaccount-text05"><span>Phone</span></span>
-                </div>
-                <input type="text" name="phone" class="createanaccount-textfield1" id="phone" value="">
-              </div>
-              <div class="createanaccount-frame358">
-                <div class="createanaccount-textfield">
-                  <div class="createanaccount-frame243">
-                    <span class="createanaccount-text05">
-                      <span>Password</span>
-                    </span>
-                    
-                  </div>
-                  <input type="password" name="password" class="createanaccount-textfield1" id="password" value="">
-                </div>
-                <div class="createanaccount-frame357">
-                  <div class="createanaccount-frame355">
-                    <div class="createanaccount-bulletpoint">
-                      <img
-                        src="external/ellipse1i103-fp6p-200h.png"
-                        alt="Ellipse1I103"
-                        class="createanaccount-ellipse1"
-                      />
-                      <span class="createanaccount-text17">
-                        <span>Use 8 or more characters</span>
-                      </span>
-                    </div>
-                    <div class="createanaccount-bulletpoint">
-                      <img
-                        src="external/ellipse1i103-cyb8-200h.png"
-                        alt="Ellipse1I103"
-                        class="createanaccount-ellipse1"
-                      />
-                      <span class="createanaccount-text17">
-                        <span>Use a number (e.g. 1234)</span>
-                      </span>
-                    </div>
-                  </div>
-                  <div class="createanaccount-frame356">
-                    <div class="createanaccount-bulletpoint">
-                      <img
-                        src="external/ellipse1i103-xdr-200h.png"
-                        alt="Ellipse1I103"
-                        class="createanaccount-ellipse1"
-                      />
-                      <span class="createanaccount-text17">
-                        <span>Use upper and lower case letters (e.g. Aa)</span>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="createanaccount-frame360">
-                <button class="createanaccount-button" type="submit">
-                  <div class="createanaccount-frame276">
-                    <span class="createanaccount-text23">
-                      <span>Daftar</span>
+              <div class="createanaccount-frame357">
+                <div class="createanaccount-frame355">
+                  <div class="createanaccount-bulletpoint">
+                    <img
+                      src="external/ellipse1i103-fp6p-200h.png"
+                      alt="Ellipse1I103"
+                      class="createanaccount-ellipse1"
+                    />
+                    <span class="createanaccount-text17">
+                      <span>Use 8 or more characters</span>
                     </span>
                   </div>
-                </button>
-                <?php if(session('error')): ?>
-                    <div class="alert alert-danger">
-                        <?php echo e(session('error')); ?>
+                  <div class="createanaccount-bulletpoint">
+                    <img
+                      src="external/ellipse1i103-cyb8-200h.png"
+                      alt="Ellipse1I103"
+                      class="createanaccount-ellipse1"
+                    />
+                    <span class="createanaccount-text17">
+                      <span>Use a number (e.g. 1234)</span>
+                    </span>
+                  </div>
+                </div>
+                <div class="createanaccount-frame356">
+                  <div class="createanaccount-bulletpoint">
+                    <img
+                      src="external/ellipse1i103-xdr-200h.png"
+                      alt="Ellipse1I103"
+                      class="createanaccount-ellipse1"
+                    />
+                    <span class="createanaccount-text17">
+                      <span>Use upper and lower case letters (e.g. Aa)</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="createanaccount-frame360">
+              <button class="createanaccount-button" type="submit">
+                <div class="createanaccount-frame276">
+                  <span class="createanaccount-text23">
+                    <span>Daftar</span>
+                  </span>
+                </div>
+              </button>
+              <?php if(session('error')): ?>
+                  <div class="alert alert-danger">
+                      <?php echo e(session('error')); ?>
 
-                    </div>
-                <?php endif; ?>
-                
-                
-              </div>
+                  </div>
+              <?php endif; ?>
+              
+              
             </div>
           </div>
         </div>
-      </form>
-    </div>
-    </div>
-  </body>
-</html>
-<?php /**PATH D:\Documents\project skripsi\with git\front end\project skripsi\laravel\front-end-project\resources\views/user/Register.blade.php ENDPATH**/ ?>
+      </div>
+    </form>
+  </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Documents\project skripsi\with git\front end\project skripsi\laravel\front-end-project\resources\views/user/Register.blade.php ENDPATH**/ ?>
