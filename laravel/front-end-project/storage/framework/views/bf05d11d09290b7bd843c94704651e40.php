46a47921577cda5805d3d1b6e7ef644cd96ef29a
albert.akira <albert.akira@gdn-commerce.com>
2024-03-19 17:18:45 +0700

    create home page

--- laravel/front-end-project/storage/framework/views/bf05d11d09290b7bd843c94704651e40.php
+++ laravel/front-end-project/storage/framework/views/bf05d11d09290b7bd843c94704651e40.php
@@ -39,72 +39,153 @@
         </span>
     </div>
     <div class="section3">
-        <div class="home-main-page-frame14287">
-          <div class="home-main-page-frame14284">
-            <div class="home-main-page-group12">
+        <div class="home-main-page-frame">
+          <div class="home-main-page-frame1">
+            <div class="home-main-page-group">
               <img
                 src="external/ellipse88637-5xc-200h.png"
                 alt="Ellipse88637"
-                class="home-main-page-ellipse8"
+                class="home-main-page-ellipse"
               />
               <img
                 src="external/bridesmaid11101-o186-200h.png"
                 alt="bridesmaid11101"
-                class="home-main-page-bridesmaid1"
+                class="home-main-page-bridesmaid"
               />
             </div>
-            <span class="home-main-page-text33"><span>40+</span></span>
-            <span class="home-main-page-text35">
+            <span class="home-main-page-text5"><span>40+</span></span>
+            <span class="home-main-page-text6">
               <span>Jumlah Usher Kami</span>
             </span>
           </div>
-          <div class="home-main-page-frame14285">
-            <div class="home-main-page-group121">
+          <div class="home-main-page-frame2">
+            <div class="home-main-page-group1">
               <img
                 src="external/ellipse88638-9s7-200h.png"
                 alt="Ellipse88638"
-                class="home-main-page-ellipse81"
+                class="home-main-page-ellipse2"
               />
               <img
                 src="external/weddingcouple11101-r8q8-200h.png"
                 alt="weddingcouple11101"
-                class="home-main-page-weddingcouple1"
+                class="home-main-page-weddingcouple"
               />
             </div>
-            <span class="home-main-page-text37"><span>200+</span></span>
-            <span class="home-main-page-text39">
+            <span class="home-main-page-text7"><span>200+</span></span>
+            <span class="home-main-page-text8">
               <span>Jumlah Klien</span>
             </span>
           </div>
-          <div class="home-main-page-frame14286">
-            <div class="home-main-page-group122">
+          <div class="home-main-page-frame3">
+            <div class="home-main-page-group2">
               <img
                 src="external/ellipse88638-gplg-200h.png"
                 alt="Ellipse88638"
-                class="home-main-page-ellipse82"
+                class="home-main-page-ellipse3"
               />
               <img
                 src="external/wedding12541-7mym-200h.png"
                 alt="wedding12541"
-                class="home-main-page-wedding1"
+                class="home-main-page-wedding"
               />
             </div>
-            <span class="home-main-page-text41"><span>100+</span></span>
-            <span class="home-main-page-text43">
+            <span class="home-main-page-text9"><span>100+</span></span>
+            <span class="home-main-page-text10">
               <span>Jumlah Venue</span>
             </span>
           </div>
         </div>
       </div>
-      <button class="home-main-page-divczmbutton">
-        <div class="home-main-page-ifawhatsapp">
-          <img src="external/wawhite.png" class="home-main-page-text45"></img>
+      <button class="home-main-page-button1">
+        <div class="home-main-page-whatsapp">
+          <img src="external/wawhite.png" class="home-main-page-text11"></img>
         </div>
-        <span class="home-main-page-text46"><span>&nbsp;Chat</span></span>
+        <span class="home-main-page-text12"><span>&nbsp;Chat</span></span>
       </button>
 </div>
 
+<div class="container2">
+  <div class="frame4">
+    <span class="text13"><span>Wedding Usher</span></span>
+    <span class="text14">
+      <span>
+        Usher Wedding adalah orang yang ditugaskan untuk bertanggung
+        jawab atas kelancaran acara pernikahan dengan tugas utamanya
+        meliputi menyambut tamu undangan, menunjukkan tempat duduk, dan
+        mengelola serta menjaga angpao yang diberikan oleh para tamu
+        kepada mempelai. Usher wedding memainkan peran kunci dalam
+        menjaga kelancaran dan keberhasilan jalannya acara pernikahan.
+      </span>
+    </span>
+  </div>
+  <span class="text15"><span>Why Choose Us?</span></span>
+  <span class="text16">
+    <span>
+      Hayden sangat memprioritaskan pengelolaan anggaran bagi pasangan
+      pengantin. Hayden mengutamakan kenyamanan dan kepercayaan klien
+      dengan komitmen memberikan layanan berkualitas optimal dengan
+      harga yang bersahabat. Tujuannya adalah untuk membantu pasangan
+      pengantin menciptakan kesan elegan dan profesional selama acara
+      pernikahan dengan bantuan ushers yang terlatih secara profesional
+      dalam bidangnya.
+    </span>
+  </span>
+  
+  <img
+    src="external/ellipse91471-qfu-300h.png"
+    alt="Ellipse91471"
+    class="frame-ellipse4"
+  />
+</div>
 
+<div class="container3">
+  <span class="text17"><span>T&amp;C Hayden Usher</span></span>
+  <span class="text18">
+    <span>
+      <span>- Usher Datang 2 Jam sebelum acara dimulai</span>
+      <br />
+      <span>- Usher Stand By 1 Jam sebelum acara</span>
+      <br />
+      <span>- Max jam kerja adalah 4 jam dihitung selama</span>
+      <br />
+      <span> &nbsp acara berlangsung</span>
+      <br />
+      <span></span>
+    </span>
+  </span>
+  <img
+    src="external/gambar41585-l4th-500h.png"
+    alt="gambar41585"
+    class="frame-image"
+  />
+</div>
+
+<div class="container4">
+  <img
+    src="external/rectangle281585-r34r-400h.png"
+    alt="Rectangle281585"
+    class="frame-rectangle"
+  />
+  <span class="text19"><span>Sistem Penomoran</span></span>
+  <img
+    src="external/kisspngredenvelopeweddingdownloadchinesenewyearred1585-dvhq-600h.png"
+    alt="kisspngredenvelopeweddingdownloadchinesenewyearred1585"
+    class="angpao-img"
+  />
+  <span class="text20">
+    <span>
+      Seorang petugas penjaga angpao akan bertugas secara konsisten dari
+      awal hingga akhir acara, hadir dan siap berada di meja tamu untuk
+      menerima dan menempatkan amplop angpao yang telah ditandai ke
+      dalam kotak angpao. Setiap amplop akan ditandai dengan stiker
+      nomor oleh Usher sebelumnya. Setelah acara selesai, proses serah
+      terima akan dilakukan langsung di hadapan pihak keluarga dan
+      wedding organizer. Kotak angpao akan dibuka dengan oleh pihak
+      keluarga, kemudian angpao akan dihitung dan disamakan secara
+      langsung saat proses serah terima.
+    </span>
+  </span>
+</div>
 
 
 
